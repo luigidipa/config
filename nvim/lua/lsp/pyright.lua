7c713d9d595b628e447125c47b161bce3cc5d012
Luigi Di Paolo <luigi.dipaolo@ibm.com>
2026-07-08 12:07:49 +0100

    Add root_markers to pyright to pick up environment base on venv

--- nvim/lua/lsp/pyright.lua
+++ nvim/lua/lsp/pyright.lua
@@ -5,6 +5,7 @@ capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 return {
   cmd = { "pyright-langserver", "--stdio" },
   filetypes = { "python" },
+  root_markers = { "pyrightconfig.json", "pyproject.toml", "setup.py", "setup.cfg", ".git" },
   on_attach = common.on_attach,
   capabilities = capabilities,
 }
